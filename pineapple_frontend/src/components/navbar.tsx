0d2ec289f8c4d507dc88bea9a20fbdb99f3beafc
emil <emilbabazade29@gmail.com>
2021-03-18 18:12:49 +0200

    navbar logo

--- pineapple_frontend/src/components/navbar.tsx
+++ pineapple_frontend/src/components/navbar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../styles/css/navbar.css'
+import PineappleLogo from './pineapple.svg'
 
 const Navbar = (): JSX.Element => (
     <nav>
         <ul>
-            <h6>pineapple</h6>
+            <h6>
+                <img src={PineappleLogo} />
+                pineapple
+            </h6>
             <li><a href="#">About</a></li>
             <li><a href="#">How it works</a></li>
             <li><a href="#">Contact</a></li>
