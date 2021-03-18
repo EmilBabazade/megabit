f35a8c4f53fcf7fde7c1548c67281b5d75552096
emil <emilbabazade29@gmail.com>
2021-03-18 19:24:28 +0200

    css checkbox work in progress

--- pineapple_frontend/src/components/newsletter.tsx
+++ pineapple_frontend/src/components/newsletter.tsx
@@ -7,8 +7,12 @@ const Newsletter = (): JSX.Element => (
         <p>Subscribe to our newsletter and get 10% discount on pineapple glasses</p>
         <form>
             <input type="text" placeholder="Type your email address here..."/>
-            <input type="checkbox" /> <p>I agree to <a href="#">terms of service</a></p>
             <button type="submit">Subscribe</button>
+            <div className="checkmark">
+                <input type="checkbox" /> 
+                <span></span>
+                <label>I agree to <a href="#">terms of service</a></label>
+            </div>
         </form>
     </section>
 )
